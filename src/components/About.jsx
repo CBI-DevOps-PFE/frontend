6359abc12ed0e1c0d27adcf343ffe731a1759bd8
najia <bounajia4@gmail.com>
2024-04-05 11:27:28 +0200

    merge 'origin/main'

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,9 @@ function About() {
     <div id='about'>
         <div className='about-image'>
            <img  src={aboutimage} alt=''/>
-
+ {/* <video width="640" height="360" controls autoPlay  loop muted plays-inline>
+        <source src=".\videos\vedio1.mp4" type="video/mp4" />
+         */}
         </div>
         <div className='about-text'>
             <h1>LEARN MORE ABOUT US</h1>
